fd4df76a37db9e2f908f7c2c97e7b2adb963d06a
kolinkuang <kolinkuang@github.com>
2021-07-12 23:04:53 +0800

    2nd commit

--- static/index.js
+++ static/index.js
@@ -1,2 +1,6 @@
-const p = document.querySelector('p');
-console.log(p);
+// var p = document.querySelector('p');
+// console.log(p);
+
+var p = document.querySelector('p');
+var style = window.getComputedStyle(p, null);
+console.log(style.color);
